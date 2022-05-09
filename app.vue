ea83548b42a3f7bdced1a0fb49ee3884f4401c3c
robigan <35210888+robigan@users.noreply.github.com>
2022-05-09 13:19:28 +0200

    Karma's a bitch

--- app.vue
+++ app.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="d-flex">
-    <div class="w-75 p-3">
-      <div class="border" style="min-height: calc(100vh - 2rem);">
-        test
+  <div class="row">
+    <div class="col p-3">
+      <div>
+        <img src="/images/monopoly.svg" class="" style="height: calc(100vh - 2rem); min-width: calc(100vh - 2rem);" />
       </div>
     </div>
-    <div class="w-25 p-3" style="min-width: 250px;">
+    <div class="col p-3" style="min-width: 250px;">
       <div class="d-flex flex-column justify-content-between align-items-center" style="min-height: calc(100vh - 2rem); gap: 1rem 0;">
         <div class="p-3 border w-100 d-flex flex-column justify-content-between align-items-centers" style="flex-grow: 3; gap: 1rem 0;">
           <div class="p-3 border w-100" style="/* flex-grow: 1; */">Card Name & Price</div>
